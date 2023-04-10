c7decbcb3e312d2b1dece38f1a184a1d34eef6e2
Perera Dev <nashi.codes@gmail.com>
2023-04-10 18:32:20 +0100

    criando projeto da aula 07

---
+++ Aula07/src/main/java/com/nashicodes/aula07/Aula07.java
@@ -0,0 +1,11 @@
+package com.nashicodes.aula07;
+
+public class Aula07 {
+
+    // Aula sobre Atributos e Métodos estaticos
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
